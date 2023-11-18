4c2dd46d5543064d53e2fd5ec5cb536303779269
ssuarezp <ssuarezp@unal.edu.co>
2023-11-18 12:15:19 -0500

    20231118 2

--- preguntas.py
+++ preguntas.py
@@ -47,6 +47,7 @@ def pregunta_02():
 
     """
     # Lea el archivo de datos
+    # Lea el archivo de datos
     df = pd.read_csv("house-votes-84.csv", sep=",")
 
     # Cree un vector con la variable de respuesta ('party')
@@ -63,7 +64,39 @@ def pregunta_02():
 
     # Importe KNeighborsClassifier de sklearn.neighbors
     from sklearn.neighbors import KNeighborsClassifier
-    
+
+    # Cree un un clasificador k-NN con 6 vecinos
+    knn = KNeighborsClassifier(n_neighbors=6)
+
+    # Entrene el clasificador con el conjunto de entrenamiento
+    knn.fit(X, y)
+
+    # Retorne el score del clasificador
+    return knn.score(X, y)
+
+
+def pregunta_02():
+    """
+    Complete el código presentado a continuación.
+
+    """
+    # Lea el archivo de datos
+    df = pd.read_csv("house-votes-84.csv", sep=",")
+
+    # Cree un vector con la variable de respuesta ('party')
+    y = df['party'].copy()
+
+    # Extraiga las variables de entrada
+    X = df.drop('party', axis=1).values
+
+    # Importe el transformador OrdinalEncoder
+    from sklearn.preprocessing import OrdinalEncoder
+
+    # Transforme las variables de entrada usando fit_transform
+    X = OrdinalEncoder().fit_transform(X)
+
+    # Importe KNeighborsClassifier de sklearn.neighbors
+    from sklearn.neighbors import KNeighborsClassifier
 
     # Cree un un clasificador k-NN con 6 vecinos
     knn = KNeighborsClassifier(n_neighbors=6)
